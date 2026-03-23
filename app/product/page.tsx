678b8eb00e56f961d34c84ddbc1ae9098d7373cc
toramarutora44-png <257756887+toramarutora44-png@users.noreply.github.com>
2026-03-23 22:32:21 +0900

    deploy

---
+++ app/product/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+export default function Product() {
+  return (
+    <main className="bg-white text-black">
+
+      {/* メイン */}
+      <img src="/1.jpg" className="w-full" />
+
+      <div className="text-center mt-4 font-bold text-xl">
+        これ、可愛すぎて無理
+      </div>
+
+      {/* 詳細 */}
+      <div className="p-4 space-y-6">
+        <img src="/2.jpg" className="rounded-xl" />
+        <img src="/3.jpg" className="rounded-xl" />
+        <img src="/4.jpg" className="rounded-xl" />
+      </div>
+
+      {/* 口コミ */}
+      <div className="p-4 text-sm space-y-1">
+        <p>・思ったよりしっかりしてる</p>
+        <p>・プレゼントで喜ばれた</p>
+        <p>・毎日使ってる</p>
+      </div>
+
+      {/* CTA */}
+      <div className="fixed bottom-0 w-full bg-black p-4">
+        <a href="https://lin.ee/wuKhILR" target="_blank">
+          <button className="w-full bg-white py-3 rounded-xl font-bold">
+            LINEで購入する
+          </button>
+        </a>
+      </div>
+
+    </main>
+  );
+}
